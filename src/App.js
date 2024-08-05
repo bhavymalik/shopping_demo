dc8ccf57dd68b506491f60971ea0d18b125b76d1
bhavy malik <bhavymalik007@gmail.com>
2024-08-05 23:27:20 +0530

    attempt at hushh button

--- src/App.js
+++ src/App.js
@@ -2,12 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
 import Content from './components/content';
+import {HushhButton} from "hushh-button"
+
 
 function App() {
+
+  const questionsArray = [
+    {
+      question: "What are your plans for today?",
+      options: ["Our products", "Our team", "Get recruited at Hushh", "Partner with us"],
+      answer: ""
+    },
+    {
+      question: "What explains you the best?",
+      options: ["User", "Agent", "Brand representative", "Fellow Developer", "Applicant"],
+      answer: ""
+    },
+    {
+      question: "What product you want to explore first?",
+      options: ["Hushh Button (This very thing :)", "Hushh Wallet App", "Hushh For Students", "Browser Companion", "Vibe Search", "Concierge App", "Valet Chat", "Hushh Developer APIs"],
+      answer: ""
+    }
+  ]
+
   return (
     <div className="App">
     <Navbar/>
     <Content/>
+    <HushhButton questions={questionsArray} />
     </div>
   );
 }
